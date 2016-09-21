40bdfee44d2a1848038c5698a0110c45f01d787b
Douglas Brion <db1415@ic.ac.uk>
2016-09-21 19:01:55 +0100

    Starting Laplacian Operator image processing for sharpening images - basic detail extraction currently

--- Laplacian/laplacian.py
+++ Laplacian/laplacian.py
@@ -7,19 +7,19 @@ from PIL import Image
 import scipy.misc
 import math
 
-# The image that you want to use the Gaussian Blur filter on
-usedImage = 'lena.jpeg'
+# The image that you want to use the Laplacian operator on
+usedImage = 'moon.jpeg'
 
 # Open the desired JPEG image as an array
 img = array(Image.open(usedImage).convert("L"))
 
-# Gaussian Blur kernel
-kernel = [[1, 1, 1],
-           [1, 1, 1],
-           [1, 1, 1]]
+# Laplacian operator
+kernel = [[-1, -1, -1],
+           [-1, 8, -1],
+           [-1, -1, -1]]
 
 # CONVOLUTION 1
-# Convolute the input image with the gaussian blur kernel
+# Convolute the input image with the laplacian kernel
 # Generate output array im of the convolution
 im = signal.convolve(img, kernel, mode='same')
 
@@ -30,4 +30,10 @@ print (im)
 # Save the arrays created as JPEG images
 scipy.misc.imsave('im.jpeg', im)
 
-print ('Finished Gaussian Blur')
+# Add the convoluted image to the original to start sharpen
+final = im + img
+
+# Save combined image
+scipy.misc.imsave('final.jpeg', final)
+
+print ('Finished Laplacian Operator')
